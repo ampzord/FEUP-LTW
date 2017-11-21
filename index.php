b478ab13db89b4fc7bedf65a76cefb9fd5f757ac
Lantaros <ruileixo@hotmail.com>
2017-11-21 19:11:04 +0000

    First commit, started html and forder structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <header>
+      <img alt="Just Do It!" src="img/logo.png">
+    </header>
+    <form id="loginForm" method="post" action="login.php">
+      <input name="username" type="text" autocomplete="true" value="Username">
+      <input name="password" type="password" autocomplete="true" value="Password">
+      <input type="submit">
+    </form>
+  </body>
+</html>
