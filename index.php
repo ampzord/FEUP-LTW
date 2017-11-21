44f10445e19f10d6ca8c43c5964708051f853812
Lantaros <ruileixo@hotmail.com>
2017-11-21 20:14:57 +0000

    Started SQL database file

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
       <img alt="Just Do It!" src="img/logo.png">
     </header>
     <form id="loginForm" method="post" action="login.php">
-      <input name="username" type="text" autocomplete="true" value="Username">
-      <input name="password" type="password" autocomplete="true" value="Password">
+      <input name="username" type="text" autocomplete="true" placeholder="Username">
+      <input name="password" type="password" autocomplete="true" placeholder="Password">
       <input type="submit">
     </form>
   </body>
