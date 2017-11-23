5639a43ff3d63f4c14bc3e240ac466dd0548ffc6
Francisco Silva <francisjssilva@gmail.com>
2017-11-23 14:47:09 +0000

    Added Register.php and Index.php
    added all css and images

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
+
     <header>
-      <img alt="Just Do It!" src="img/logo.png">
+      <img alt="Just Do It!" style="width: 200px;" src="img/logo.png">
     </header>
-    <form id="loginForm" method="post" action="login.php">
-      <input name="username" type="text" autocomplete="true" placeholder="Username">
-      <input name="password" type="password" autocomplete="true" placeholder="Password">
-      <input type="submit">
-    </form>
+
+    <br><br><br><br><br><br><br><br>
+
+    <div class="content">
+      <div class="presentationBox">
+        Need to save your <b>tasks</b>?<br> You can do it now in a safe place!
+      </div>
+
+      <div class="loginForm">
+        <form id="loginForm" method="post" action="login.php">
+          <input name="username" type="text" autocomplete="true" placeholder="Username">
+          <input name="password" type="password" autocomplete="true" placeholder="Password">
+          <input type="submit" value="Login">
+        </form>
+        <button class="backbt" onclick="window.location.href='register.php'">Register</button>
+      </div>
+    </div>
+
+
+    <footer>
+      <br>
+      <a href="https://github.com/ampzord/FEUP-LTW">FEUP-LTW 2017-2018</a><br>
+      Francisco Silva | Rui Leixo | António | Todos os direitos reservados
+    </footer>
+
   </body>
 </html>
