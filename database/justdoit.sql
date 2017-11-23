5d919d8150256f1c88c465094f6740e025572c31
António Pereira <antonio_m_sp@hotmail.com>
2017-11-23 10:59:05 +0000

    Corrected database and added admin.

--- database/justdoit.sql
+++ database/justdoit.sql
@@ -1,35 +1,43 @@
+PRAGMA FOREIGN_KEYS = ON;
+.mode columns
+.headers on
+.nullvalue NULL
+
 DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Group;
+DROP TABLE IF EXISTS Team;
 DROP TABLE IF EXISTS List;
 DROP TABLE IF EXISTS Task;
 
-
-CREATE TABLE User {
+CREATE TABLE User (
   id INTEGER PRIMARY KEY,
   username TEXT UNIQUE,
-  email TEXT PRIMARY KEY,
+  email TEXT UNIQUE,
   phoneNumber INTEGER UNIQUE,
   country TEXT UNIQUE,
-  timeZone INTEGER NOT UNIQUE --UTC Offset
+  timeZone INTEGER, --UTC Offset
   fullName TEXT NOT NULL,
-  birthDate DATE NOT NULL,
+  birthDate DATE,
   passwordHash TEXT NOT NULL
-};
+);
 
-CREATE TABLE Group {
+CREATE TABLE Team (
   id INTEGER PRIMARY KEY,
   name TEXT UNIQUE,
-  idUser INTEGER REFERENCES User (id)
-};
+  idUser INTEGER REFERENCES User(id)
+);
 
-CREATE TABLE List {
+CREATE TABLE List
+(
   id INTEGER PRIMARY KEY,
   name TEXT UNIQUE,
-  idGroup INTEGER REFERENCES Group (id)
-};
+  idGroup INTEGER REFERENCES Team(id)
+);
 
-
-CREATE TABLE Task {
+CREATE TABLE Task
+(
   id INTEGER PRIMARY KEY,
-  idList INTEGER REFERENCES List (id)
-};
+  idList INTEGER REFERENCES List(id)
+);
+
+-- Admin
+INSERT INTO User(id, username, fullName, passwordHash) VALUES (1, "admin", "Malaquias","a");
