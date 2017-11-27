fd0654d78df8afe5f18c5919136bf531fdd2d2fe
Francisco Silva <francisjssilva@gmail.com>
2017-11-27 17:12:29 +0000

    added interface with navBar
    added interface with navBar

---
+++ interface.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+
+    <header class="interfaceHeader">
+      <div class="headerContent">
+
+        <table>
+          <tr>
+            <td>
+              <button id="addButton"></button>
+            </td>
+            <td>
+              <form class="navBarform" method="get" action="search.php">
+                <input type="text" name="search" placeholder="Wazzup?!" id="searchButton"></input>
+              </form>
+            </td>
+            <td style="width:100%;">
+              <img alt="Just Do It!" style="width: 70px;" src="img/logoInterface.png">
+            </td>
+            <td>
+              <button id="notificationButton"></button>
+            </td>
+            <td>
+              <button id="profileButton">F</button>
+            </td>
+          </tr>
+        </table>
+
+      </div>
+    </header>
+
+    <br><br><br><br><br><br><br><br>
+
+    <div class="content">
+
+    </div>
+
+
+    <footer>
+      <br><br>
+      <a href="https://github.com/ampzord/FEUP-LTW">FEUP-LTW 2017-2018</a><br>
+      Francisco Silva | Rui Leixo | António | Todos os direitos reservados
+    </footer>
+
+  </body>
+</html>
