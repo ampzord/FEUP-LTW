698ef563e552422f3dd037fbf7f9b740c8608ab0
AMPZORD <antonio_m_sp@hotmail.com>
2017-12-14 01:38:39 +0000

    Forgot saving logout.php file.

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?
-<<<<<<< HEAD
-  session_set_cookie_params (432000, '/~up201307910/justdoit', 'gnomo.fe.up.pt', true, true);  
-=======
   // include_once('')
   /*session_destroy();
   unset($_SESSION['username']);
   session_start();*/
 
   //session_set_cookie_params (432000, '/~up201502860/FEUP-LTW', 'gnomo.fe.up.pt', true,  true);
->>>>>>> c8b4d0ae20b8a6ffe65286b448af86729137670d
   session_start();
   session_destroy();
   unset($_SESSION);
