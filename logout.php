c8b4d0ae20b8a6ffe65286b448af86729137670d
francisjssilva <francisjssilva@gmail.com>
2017-12-14 01:32:38 +0000

    kjasdhkj

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@
   unset($_SESSION['username']);
   session_start();*/
 
-  session_set_cookie_params (432000, '/~up201307910/justdoit', 'gnomo.fe.up.pt', true, true);  
+  //session_set_cookie_params (432000, '/~up201502860/FEUP-LTW', 'gnomo.fe.up.pt', true,  true);
   session_start();
   session_destroy();
   unset($_SESSION);
